ddb0ff62aa82d6c4507c9db7557aa3181217309e
takakotmt <st.0614.tmt@gmail.com>
2021-12-26 13:53:25 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to root_path
+  end
+
+  private
+  def post_params
+		params.require(:post).permit(:description)
+	end
+end
